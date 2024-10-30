66bde0950914736d26b654b6f9119b12a8c36247
Aswany-Santhosh <chn23mca037@gmail.com>
2024-10-30 22:20:43 +0530

    account information

--- users/views.py
+++ users/views.py
@@ -4,10 +4,16 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
+    print(request.user)
     return render(request,'users/index.html')
 
 def profile(request):
     return render(request,'users/profile.html')
 
-def account(request):
-    return render(request,'users/account.html')
+def account(request): 
+    user = request.user
+    print(user.email)
+    context = {
+        'user_email' : user
+    }
+    return render(request,'users/account.html',context)
